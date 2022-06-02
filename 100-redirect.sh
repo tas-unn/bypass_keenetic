f01a01f55cbbd7011073cc249799ce9ecb318e58
tas-unn <92320184+tas-unn@users.noreply.github.com>
2022-06-02 13:04:03 +0300

    Update 100-redirect.sh

--- 100-redirect.sh
+++ 100-redirect.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 [ "$type" == "ip6tables" ] && exit 0
 if [ -z "$(iptables-save 2>/dev/null | grep unblocksh)" ]; then
-    ipset create unblocksh hash:net family inet -exist
+    ipset create unblocksh hash:net -exist
     iptables -I PREROUTING -w -t nat -i br0 -p tcp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
     iptables -I PREROUTING -w -t nat -i br0 -p udp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
-	iptables -t nat -A OUTPUT -p tcp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
+....
+     iptables -I PREROUTING -w -t nat -i sstp0 -p tcp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
+    iptables -I PREROUTING -w -t nat -i sstp0 -p udp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
+    iptables -t nat -A OUTPUT -p tcp -m set --match-set unblocksh dst -j REDIRECT --to-port 1082
 fi
 if [ -z "$(iptables-save 2>/dev/null | grep "udp \-\-dport 53 \-j DNAT")" ]; then
     iptables -w -t nat -I PREROUTING -i br0 -p udp --dport 53 -j DNAT --to 192.168.1.1
+    iptables -w -t nat -I PREROUTING -i sstp0 -p udp --dport 53 -j DNAT --to 192.168.1.1
+....
 fi
 if [ -z "$(iptables-save 2>/dev/null | grep "tcp \-\-dport 53 \-j DNAT")" ]; then
     iptables -w -t nat -I PREROUTING -i br0 -p tcp --dport 53 -j DNAT --to 192.168.1.1
+    iptables -w -t nat -I PREROUTING -i sstp0 -p tcp --dport 53 -j DNAT --to 192.168.1.1
 fi
 if [ -z "$(iptables-save 2>/dev/null | grep unblocktor)" ]; then
-    ipset create unblocktor hash:net family inet -exist
-    iptables -I PREROUTING -w -t nat -i br1 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
-    iptables -I PREROUTING -w -t nat -i br1 -p udp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
-	iptables -t nat -A PREROUTING -i br1 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
-	iptables -t nat -A OUTPUT -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
-fi
-if [ -z "$(iptables-save 2>/dev/null | grep "udp \-\-dport 53 \-j DNAT")" ]; then
-    iptables -w -t nat -I PREROUTING -i br1 -p udp --dport 53 -j DNAT --to 192.168.1.1
-fi
-if [ -z "$(iptables-save 2>/dev/null | grep "tcp \-\-dport 53 \-j DNAT")" ]; then
-    iptables -w -t nat -I PREROUTING -i br1 -p tcp --dport 53 -j DNAT --to 192.168.1.1
+    ipset create unblocktor hash:net -exist
+    iptables -I PREROUTING -w -t nat -i br0 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+    iptables -I PREROUTING -w -t nat -i br0 -p udp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+    iptables -t nat -A PREROUTING -i br0 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+    iptables -t nat -A OUTPUT -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+....
+    iptables -I PREROUTING -w -t nat -i sstp0 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+    iptables -I PREROUTING -w -t nat -i sstp0 -p udp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+    iptables -t nat -A PREROUTING -i sstp0 -p tcp -m set --match-set unblocktor dst -j REDIRECT --to-port 9141
+....
 fi
 exit 0
