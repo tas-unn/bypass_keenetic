04bdd6d07359f50a53d6346ccd583a3577266174
tas-unn <92320184+tas-unn@users.noreply.github.com>
2022-07-12 08:37:49 +0300

    Create add100redirect.sh

---
+++ add100redirect.sh
@@ -0,0 +1,13 @@
+if [ -z "$(iptables-save 2>/dev/null | grep unblockvpn)" ]; then
+    ipset create unblockvpn hash:net -exist
+    iptables -I PREROUTING -w -t nat -i br0 -p tcp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+    iptables -I PREROUTING -w -t nat -i br0 -p udp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+    iptables -t nat -A PREROUTING -i br0 -p tcp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+    iptables -t nat -A OUTPUT -p tcp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+
+    iptables -I PREROUTING -w -t nat -i sstp0 -p tcp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+    iptables -I PREROUTING -w -t nat -i sstp0 -p udp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+    iptables -t nat -A PREROUTING -i sstp0 -p tcp -m set --match-set unblockvpn dst -j MASQUERADE -o ppp1
+
+fi
+exit 0
