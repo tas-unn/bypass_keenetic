46f380a918128242a0a06baef84c07f104cc2964
tas-unn <92320184+tas-unn@users.noreply.github.com>
2022-07-19 17:22:46 +0300

    Update unblock_update.sh

--- unblock_update.sh
+++ unblock_update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ipset flush unblocktor
 ipset flush unblocksh
+#ipset flush unblockvpn # добавляем столько сколько надо
 /opt/bin/unblock_dnsmasq.sh
 /opt/etc/init.d/S56dnsmasq restart
 /opt/bin/unblock_ipset.sh &
