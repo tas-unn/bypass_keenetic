207040d395de9761c096bbfb2c8c30f822da7d38
tas-unn <92320184+tas-unn@users.noreply.github.com>
2023-02-27 17:10:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,19 +10,14 @@ import shutil
 import datetime
 import requests
 import json
+from bot_config import token, appapiid, appapihash, usernames
+
 # ЕСЛИ ВЫ ХОТИТЕ ПОДДЕРЖАТЬ РАЗРАБОТЧИКА - МОЖЕТЕ ОТПРАВИТЬ ДОНАТ НА ЛЮБУЮ СУММУ
 # 2204 1201 0098 8217 КАРТА МИР
 # 410017539693882 Юмани
 # bc1qesjaxfad8f8azu2cp4gsvt2j9a4yshsc2swey9  Биткоин кошелёк
 
-# ВЕРСИЯ СКРИПТА 2.0
-token='MyTokenFromBotFather' # ключ апи бота
-usernames=[]
-usernames.append('Mylogin') # Добавляем логины телеграма для администраторирования бота. Строчек может быть несколько
-# следующие две строки заполняются с сайта https://my.telegram.org/apps
-# вместо вас запрос будет посылать бот, оттуда и будут запрашиваться ключи
-appapiid='myapiid'
-appapihash='myiphash'
+# ВЕРСИЯ СКРИПТА 2.01
 
 # следующие настройки могут быть оставлены по умолчанию, но можно будет что-то поменять
 routerip='192.168.1.1' # ip роутера
