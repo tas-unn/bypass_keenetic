b4b0ea70d3f55e4d538c60c470bf47b8ad9fcaa6
NetworK <network-samara@mail.ru>
2023-03-02 08:46:03 +0200

    add setting ports on config

--- bot_config.py
+++ bot_config.py
@@ -1,8 +1,19 @@
 # ВЕРСИЯ СКРИПТА 2.0
-token = 'MyTokenFromBotFather'  # ключ апи бота
-usernames = ['Mylogin']
+
+token = 'MyBotFatherToken'  # ключ api бота
+usernames = ['MySuperLogin']  # ваш логин в телеграмме без @
+
 # следующие две строки заполняются с сайта https://my.telegram.org/apps
 # вместо вас запрос будет посылать бот, оттуда и будут запрашиваться ключи
 appapiid = 'myapiid'
 appapihash = 'myiphash'
 routerip = '192.168.1.1'  # ip роутера
+
+# следующие настройки могут быть оставлены по умолчанию, но можно будет что-то поменять
+localportsh = '1082'  # локальный порт для shadowsocks
+dnsporttor = '9053'  # чтобы onion сайты открывался через любой браузер - любой открытый порт
+localporttor = '9141'  # локальный порт для тор
+localportvmess = '10810'  # локальный порт для shadowsocks
+localporttrojan = '10810'  # локальный порт для shadowsocks
+dnsovertlsport = '40500'  # можно посмотреть номер порта командой "cat /tmp/ndnproxymain.stat"
+dnsoverhttpsport = '40508'  # можно посмотреть номер порта командой "cat /tmp/ndnproxymain.stat"
